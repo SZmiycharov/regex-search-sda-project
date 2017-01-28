c1d7a530dc1b0fcaa8574794983447676a81d333
Stanislav Zmiycharov <s.zmiycharov@gmail.com>
2017-01-28 19:03:23 +0200

    infix to postfix regex string function ready

--- regex-search-SDA-Project.cpp
+++ regex-search-SDA-Project.cpp
@@ -13,10 +13,136 @@
 */
 
 #include "HelperFunctions.h"
+using namespace std;
 
-int main(int argc, char* argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+* Convert infix regexp regexString to postfix notation.
+* Insert . as explicit concatenation operator.
+* Cheesy parser, return static buffer.
+*/
+void re2post(char *regexString, char* postRegex, int &lengthPostRegex)
 {
-	validateCmdParams(argc, argv);
+	int nalternation, natom;
+
+	struct {
+		int nalternation;
+		int natom;
+	} parentheses[100], *p;
+
+	p = parentheses;
+	nalternation = 0;
+	natom = 0;
+	
+	for (int i = 0; regexString[i] != NULL; i++)
+	{
+		switch (regexString[i])
+		{
+			case '(':
+				if (natom > 1){
+					--natom;
+					postRegex[lengthPostRegex] = '.';
+					++lengthPostRegex;
+				}
+				if (p >= parentheses + 100)
+					return;
+				p->nalternation = nalternation;
+				p->natom = natom;
+				p++;
+				nalternation = 0;
+				natom = 0;
+				break;
+			case '|':
+				if (natom == 0)
+					return;
+				while (--natom > 0)
+				{
+					postRegex[lengthPostRegex] = '.';
+					++lengthPostRegex;
+				}
+					
+				nalternation++;
+				break;
+			case ')':
+				if (p == parentheses)
+					return;
+				if (natom == 0)
+					return;
+				while (--natom > 0)
+				{
+					postRegex[lengthPostRegex] = '.';
+					++lengthPostRegex;
+				}
+				for (; nalternation > 0; nalternation--)
+				{
+					postRegex[lengthPostRegex] = '|';
+					++lengthPostRegex;
+				}
+					
+				--p;
+				nalternation = p->nalternation;
+				natom = p->natom;
+				natom++;
+				break;
+			case '*':
+			case '+':
+			case '?':
+				if (natom == 0)
+					return;
+				postRegex[lengthPostRegex] = regexString[i];
+				++lengthPostRegex;
+				break;
+			case '.':
+				if (natom > 1){
+					--natom;
+
+					postRegex[lengthPostRegex] = '.';
+					++lengthPostRegex;
+				}
+				natom++;
+				break;
+			default:
+				if (natom > 1){
+					--natom;
+					postRegex[lengthPostRegex] = '.';
+					++lengthPostRegex;
+				}
+				postRegex[lengthPostRegex] = regexString[i];
+				++lengthPostRegex;
+
+				natom++;
+				break;
+		}
+	}
+	if (p != parentheses)
+		return;
+	while (--natom > 0)
+	{
+		postRegex[lengthPostRegex] = '.';
+		++lengthPostRegex;
+	}
+	for (; nalternation > 0; nalternation--)
+	{
+		postRegex[lengthPostRegex] = '|';
+		++lengthPostRegex;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	char postRegex[100];
+	int lengthPostRegex = 0;
+	re2post("(a|s)*d", postRegex, lengthPostRegex);
+	cout << "*****";
+	for (int i = 0; i < lengthPostRegex; i++)
+	{
+		cout << postRegex[i];
+	}
+	cout << "****";
+
 
 	system("pause");
 	return 0;
